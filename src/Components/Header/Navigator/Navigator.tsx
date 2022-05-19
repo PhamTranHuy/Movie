c58172065b9d4548999742d40074f1e5aa1d25d5
Phạm Trần Huy <>
2022-05-19 10:33:49 +0700

    update header styles

--- src/Components/Header/Navigator/Navigator.tsx
+++ src/Components/Header/Navigator/Navigator.tsx
@@ -1,7 +1,7 @@
 export default function Navigator () {
   return (
     <nav>
-        <ul className='flex-between'>
+        <ul className='flex-between child:mr-6'>
             <li>Movies</li>
             <li>TV Shows</li>
             <li>People</li>
