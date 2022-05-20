a05ef6865c69a639afd25abf2f10f54ac04e88f5
Phạm Trần Huy <>
2022-05-20 11:39:19 +0700

    create search box

--- src/Components/Home/WelcomeBanner/WelcomeBanner.tsx
+++ src/Components/Home/WelcomeBanner/WelcomeBanner.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import SeachBox from "./SeachBox"
 
 function WelcomeBanner() {
   return (
     <section style={{backgroundImage: `linear-gradient(to right, rgba(3, 37, 65, 0.8) 0%, rgba(3, 37, 65, 0) 100%),
         url(${process.env.PUBLIC_URL + '/Home/welcome-benner-bg.jpeg'})`}} 
         className="h-80 flex-left-bottom text-white bg-top bg-cover">
-        <div className="pb-12 px-10">
+        <div className="w-full pb-12 px-10">
             <div className="mb-12">
                 <h1 className="font-bold text-5xl">Welcome.</h1>
                 <h2 className="font-semibold text-3xl">Millions of movies, TV shows and people to discover. Explore now.</h2>
             </div>
-            <div>Search Box</div>
+            <SeachBox />
         </div>
     </section>
   )
